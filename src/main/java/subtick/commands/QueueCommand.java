818c8242185f916d757d05c52bedb5d126eb8166
Ellie <intricatebread@gmail.com>
2023-05-07 10:32:01 -0400

    Fix broken brigadier moment

--- src/main/java/subtick/commands/QueueCommand.java
+++ src/main/java/subtick/commands/QueueCommand.java
@@ -31,13 +31,27 @@ public class QueueCommand
     dispatcher.register(
       literal("queueStep")
       .then(argument("queue", word()).suggests((c, b) -> suggest(Queues.getQueues(), b))
-        .then(argument("mode", word()).suggests((c, b) -> suggest(new HashSet<String>()
-          {{
-            Set<String> modes = SubTick.getTickHandler(c).queues.byCommandKey(getString(c, "queue")).getModes();
+        .then(argument("count", integer(1)).suggests((c, b) -> suggest(new HashSet<String>()
+        // HACKY FIX because brigadier doesn't like 2 arguments both suggesting stuff
+        {{
+          Set<String> modes = SubTick.getTickHandler(c).queues.byCommandKey(getString(c, "queue")).getModes();
+          if(modes.isEmpty())
+            add("1");
+          else
             addAll(modes);
-            if(modes.isEmpty())
-              add("1");
-          }}.toArray(new String[0]), b))
+         }}.toArray(new String[0]), b))
+          .then(argument("range", integer(-1, 46340)).suggests((c, b) -> suggest(new String[]{"-1", "32"}, b))
+            .then(literal("force")
+              .executes((c) -> step(c, getString(c, "queue"), "", getInteger(c, "count"), getInteger(c, "range"), true))
+            )
+            .executes((c) -> step(c, getString(c, "queue"), "", getInteger(c, "count"), getInteger(c, "range"), false))
+          )
+          .then(literal("force")
+            .executes((c) -> step(c, getString(c, "queue"), "", getInteger(c, "count"), Settings.subtickDefaultRange, true))
+          )
+          .executes((c) -> step(c, getString(c, "queue"), "", getInteger(c, "count"), Settings.subtickDefaultRange, false))
+        )
+        .then(argument("mode", word())
           .then(argument("count", integer(1)).suggests((c, b) -> suggest(new String[]{"1"}, b))
             .then(argument("range", integer(-1, 46340)).suggests((c, b) -> suggest(new String[]{"-1", "32"}, b))
               .then(literal("force")
@@ -55,21 +69,9 @@ public class QueueCommand
           )
           .executes((c) -> step(c, getString(c, "queue"), getString(c, "mode"), 1, Settings.subtickDefaultRange, false))
         )
-        .then(argument("count", integer(1))
-          .then(argument("range", integer(-1, 46340)).suggests((c, b) -> suggest(new String[]{"-1", "32"}, b))
-            .then(literal("force")
-              .executes((c) -> step(c, getString(c, "queue"), "", getInteger(c, "count"), getInteger(c, "range"), true))
-            )
-            .executes((c) -> step(c, getString(c, "queue"), "", getInteger(c, "count"), getInteger(c, "range"), false))
-          )
-          .then(literal("force")
-            .executes((c) -> step(c, getString(c, "queue"), "", getInteger(c, "count"), Settings.subtickDefaultRange, true))
-          )
-          .executes((c) -> step(c, getString(c, "queue"), "", getInteger(c, "count"), Settings.subtickDefaultRange, false))
+        .then(literal("force")
+          .executes((c) -> step(c, getString(c, "queue"), "", 1, Settings.subtickDefaultRange, true))
         )
-        // .then(literal("force")
-        //   .executes((c) -> step(c, getString(c, "queue"), "", 1, Settings.subtickDefaultRange, true))
-        // )
         .executes((c) -> step(c, getString(c, "queue"), "", 1, Settings.subtickDefaultRange, false))
       )
     );
